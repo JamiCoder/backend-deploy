afac13114c2c98150a4b61d83d3d6edb6c1370b3
Jamike Michel Ohia <jamikeohia@gmail.com>
2025-06-13 03:23:02 -0600

    Changes to settings (toggle debug )

--- nutriflex_api/settings.py
+++ nutriflex_api/settings.py
@@ -28,7 +28,9 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+if os.getenv('DEBUG') == 'True':
+    DEBUG = True
 
 ALLOWED_HOSTS = [   
     'nutriflex-ai.up.railway.app'
